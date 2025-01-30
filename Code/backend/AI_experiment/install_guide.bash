30c627528613b5147094388e072b2ba22c4cd892
QhtIsNotAvailable <qht22@mails.tsinghua.edu.cn>
2025-01-30 21:06:28 +0800

    AI_experiment v0.1debug

--- Code/backend/AI_experiment/install_guide.bash
+++ Code/backend/AI_experiment/install_guide.bash
@@ -1,10 +1,28 @@
-# 创建虚拟环境（推荐）
-conda create -n script_ai python=3.8
+# 一、创建虚拟环境（推荐）
+conda create -n script_ai python=3.11 # 创建虚拟环境，请勿在运行后续代码后重复创建，否则会直接清空所有已安装的包
 conda activate script_ai
 
-# 安装Hugging Face Transformers + PyTorch
+# 二、安装Hugging Face Transformers + PyTorch
+
+nvidia-smi # 如果你是Nvidia显卡，查看 Cuda Version
+# CUDA 12.x
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+# CUDA 11.8
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# 并不知道其他配置怎么安装，可能默认最新版？
+
 pip install transformers[torch]  # 自动匹配CUDA版本
 pip install datasets             # 数据处理工具
+# 如果安装失败，可以尝试以下命令（镜像源）
+pip install transformers[torch] -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn # 清华真拉，镜像都连不上 
+pip install transformers[torch] -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com # 还是阿里比较给力
+pip install datasets -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn 
+pip install datasets -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com 
 
-# 运行 install_test.py 测试是否安装成功
-# 运行 model_test.py 测试是否能够加载最基本的模型
+# 三、安装后测试
+# cd 到 AI_experiment 文件夹，在此目录下执行
+cd SOS/Code/backend/AI_experiment
+
+python install_test.py # 测试是否安装成功及查看电脑参数
+python model_test.py # 测试是否能够加载最基本的模型
---
+++ Code/backend/AI_experiment/path_test.py
@@ -0,0 +1,6 @@
+import torch
+print(torch.cuda.is_available())
+
+import sys
+print(sys.executable)  # 输出当前Python解释器路径
+print(sys.path)   # 输出当前Python解释器搜索路径
